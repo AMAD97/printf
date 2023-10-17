22d2cde227f57447cf88482186daf4a403e7a3fc
AMAD97 <aminedaoui00@gmail.com>
2023-10-17 16:07:03 -0700

    add printf.c file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+
+int write_char(char c);
+
+int write_str(const char *str);
+
+int write_int(int num);
+
+#endif /* MAIN_H */
